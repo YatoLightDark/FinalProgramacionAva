77ad6f6cde76afdd10a8c4ac79ac22a26fbcaefd
YatoLightDark <124542412+YatoLightDark@users.noreply.github.com>
2023-05-20 22:37:44 -0500

    Add files via upload

---
+++ Java/ProyectoProgramacion/src/Obra.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Obra   {
+    //Atributos
+    private String titulo;
+    private String tecnica;
+    private int ano;
+    private float precio;
+    private Artista artista;
+    //Constructor
+    public Obra(String titulo, String tecnica, int ano, float precio, Artista artista) {
+        this.titulo = titulo;
+        this.tecnica = tecnica;
+        this.ano = ano;
+        this.precio = precio;
+        this.artista = artista;
+
+    }
+   //Setters y Getters
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTecnica() {
+        return tecnica;
+    }
+
+    public void setTecnica(String tecnica) {
+        this.tecnica = tecnica;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public Artista getArtista() {
+        return artista;
+    }
+
+    public void setArtista(Artista artista) {
+        this.artista = artista;
+    }
+}
